c55f102f614ce60f28f08552f07ec76b22356059
JaNith-Pro <52545235+JaNith-Pro@users.noreply.github.com>
2020-07-30 21:23:01 +0530

    scala_assignment_5 question 1,2

---
+++ first.scala
@@ -0,0 +1,27 @@
+
+
+object first{
+     def main(args:Array[String]):Unit={
+         val x = new Rational(1,3)
+         val y = new Rational(5,7)
+         val z = new Rational(3,2) 
+         
+        println(x.sub(y).sub(z))
+     }
+}
+
+class Rational (x:Int,y:Int){
+      def numer = x
+      def demom = y
+    
+    
+     def add(r:Rational) = new Rational(this.numer*r.demom + r.numer*this.demom,r.demom*this.demom)
+
+     def neg:Rational = new Rational(-numer,demom)
+
+     def sub(r:Rational) = add(r.neg)
+
+     override def toString = numer + "/" + demom
+    
+
+}
